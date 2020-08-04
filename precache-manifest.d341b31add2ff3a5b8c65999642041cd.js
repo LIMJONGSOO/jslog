c920c1d008e1bd7f17d9e7bc9f951e2ab84766fd
LIMJONGSOO <limjs4535@gmail.com>
2020-08-05 00:49:19 +0900

    Updates

---
+++ precache-manifest.d341b31add2ff3a5b8c65999642041cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "906fd474ada04e1ddcdf4455089f3d73",
+    "url": "/jslog/index.html"
+  },
+  {
+    "revision": "9c70115f9a09c2be81c9",
+    "url": "/jslog/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "de388b3ef7fe0b6b355e",
+    "url": "/jslog/static/js/2.ea0056fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jslog/static/js/2.ea0056fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c70115f9a09c2be81c9",
+    "url": "/jslog/static/js/main.65c978ea.chunk.js"
+  },
+  {
+    "revision": "a4654a57544f8b454b7c",
+    "url": "/jslog/static/js/runtime-main.af448551.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jslog/static/media/logo.5d5d9eef.svg"
+  }
+]);
